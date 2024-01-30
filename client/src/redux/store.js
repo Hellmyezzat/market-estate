d6e357428c46cc8cef7b677c9c8b6bbc06ed706e
Helmy Ezzat <hellmyezzat@gmail.com>
2024-01-30 07:21:36 +0200

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,18 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './features/user/userSlice'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import userReducer from './features/user/userSlice' // Update the path to your userSlice
 
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
+
+//  Combine reducers
+const rootReducer = combineReducers({
+  user: userReducer,
+})
+
+// Configuration for redux-persist storage
+const persistConfig = {
+  key: 'root', // Primary key for persisting the state
+  storage, // Type of storage used (e.g., localStorage or sessionStorage)
+  version: 1, // Version to trigger rehydration when changes occur
+}
+
+// Create a new reducer using redux-persist
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// Configure the Redux store
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer, // Use the persisted reducer
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }),
+    getDefaultMiddleware({ serializableCheck: false }), // Ignore serializable check for Redux Toolkit
 })
 
-// You can also use getState() directly to get the current state
-console.log('Current state:', store.getState())
+// Create a persistor for the store
+export const persistor = persistStore(store)
 
-// Use subscribe to monitor changes in the state
-store.subscribe(() => {
-  console.log('State updated:', store.getState())
-})
+// // You can also use getState() directly to get the current state
+// console.log('Current state:', store.getState())
+
+// // Use subscribe to monitor changes in the state
+// store.subscribe(() => {
+//   console.log('State updated:', store.getState())
+// })
