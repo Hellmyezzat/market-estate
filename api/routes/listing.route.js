e03d3bbd528737043464696154ca578303ad6310
Helmy Ezzat <hellmyezzat@gmail.com>
2024-02-11 16:41:36 +0200

    1 - create search api route and querying listings from the database using the find method, with search, filter, and sort criteria

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,10 +3,10 @@ import {
   createListing,
   deleteListing,
   getListing,
+  getListings,
   updateListing,
 } from '../controllers/listing.controller.js'
 import { verifyToken } from '../utils/verifyUser.js'
-
 
 const router = express.Router()
 
@@ -14,5 +14,6 @@ router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
 router.post('/update/:id', verifyToken, updateListing)
 router.get('/get/:id', getListing)
+router.get('/get', getListings)
 
 export default router
